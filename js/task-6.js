00bf48f7c65b610cf7c5c5224c0e3a663e839851
Yaroslav Orlov <159253787+YaroslavOrlov17@users.noreply.github.com>
2024-06-28 11:45:20 +0300

    6 start

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const box = document.querySelector("#boxes");
+
+const input = document.querySelector("#controls input");
+
+const createButton = document.querySelector("button[data-create]");
+
+const destroyButton = document.querySelector("button[data-destroy]");
+
+createButton.addEventListener("click", haldleClickCreate);
+
+function haldleClickCreate(event) {
+  box.createElement((div.style.cssText = "width: 30px"), "height = 30px", "background-color");
+}
