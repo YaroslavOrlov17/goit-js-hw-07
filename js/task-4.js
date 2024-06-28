a32eb738e3b2ec3b3243188744d1e94dfcfae991
Yaroslav Orlov <159253787+YaroslavOrlov17@users.noreply.github.com>
2024-06-28 11:14:19 +0300

    1,2,3,4,5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const elements = event.currentTarget.elements;
+
+  const values = {
+    email: elements.email.value.trim(),
+    password: elements.password.value.trim(),
+  };
+  if (!values.email || !values.password) {
+    window.alert("All form fields must be filled in");
+  } else {
+    console.log(values);
+  }
+  event.currentTarget.reset();
+}
